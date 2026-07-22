12a9ccca2b4140f17c368d01a0d8d48621442de8
ToePyue <tangdubiaotang@gmail.com>
2026-07-22 21:02:20 +0800

    Add files via upload

---
+++ kernel/kalloc.c
@@ -0,0 +1,116 @@
+// Physical memory allocator, for user processes,
+// kernel stacks, page-table pages,
+// and pipe buffers. Allocates whole 4096-byte pages.
+
+#include "types.h"
+#include "param.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "riscv.h"
+#include "defs.h"
+
+void freerange(void *pa_start, void *pa_end);
+
+extern char end[]; // first address after kernel.
+                   // defined by kernel.ld.
+
+struct run {
+  struct run *next;
+};
+
+struct {
+  struct spinlock lock;
+  struct run *freelist;
+} kmem;
+
+struct {
+  struct spinlock lock;
+  int count[PHYSTOP / PGSIZE]; // 记录每个物理页的引用次数
+} ref;
+
+void
+kinit()
+{
+  initlock(&kmem.lock, "kmem");
+  initlock(&ref.lock, "ref"); // 新增：初始化引用计数锁
+  freerange(end, (void*)PHYSTOP);
+}
+
+void
+freerange(void *pa_start, void *pa_end)
+{
+  char *p;
+  p = (char*)PGROUNDUP((uint64)pa_start);
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+    kfree(p);
+}
+
+// Free the page of physical memory pointed at by pa,
+// which normally should have been returned by a
+// call to kalloc().  (The exception is when
+// initializing the allocator; see kinit above.)
+void
+kfree(void *pa)
+{
+  struct run *r;
+
+  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+    panic("kfree");
+
+  //递减引用计数
+  acquire(&ref.lock);
+  if(ref.count[(uint64)pa / PGSIZE] > 0) {
+    ref.count[(uint64)pa / PGSIZE] -= 1;
+  }
+  int count = ref.count[(uint64)pa / PGSIZE];
+  release(&ref.lock);
+
+  if(count > 0) return; // 如果还有其他进程在使用，直接返回，不真正释放
+
+  // Fill with junk to catch dangling refs.
+  memset(pa, 1, PGSIZE);
+
+  r = (struct run*)pa;
+
+  acquire(&kmem.lock);
+  r->next = kmem.freelist;
+  kmem.freelist = r;
+  release(&kmem.lock);
+}
+
+// Allocate one 4096-byte page of physical memory.
+// Returns a pointer that the kernel can use.
+// Returns 0 if the memory cannot be allocated.
+void *
+kalloc(void)
+{
+  struct run *r;
+
+  acquire(&kmem.lock);
+  r = kmem.freelist;
+  if(r)
+    kmem.freelist = r->next;
+  release(&kmem.lock);
+
+  if(r){
+    memset((char*)r, 5, PGSIZE); // fill with junk
+  //分配出新页面时，引用计数设为 1
+    acquire(&ref.lock);
+    ref.count[(uint64)r / PGSIZE] = 1;
+    release(&ref.lock);
+  }
+
+  return (void*)r;
+}
+
+// 新增：供其他文件调用的增加引用计数的接口
+void
+kaddref(void *pa)
+{
+  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+    panic("kaddref");
+  
+  acquire(&ref.lock);
+  ref.count[(uint64)pa / PGSIZE] += 1;
+  release(&ref.lock);
+}
